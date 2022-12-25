a8ccb52d0875f4d2f8925b5c82eb897b99a35bda
rtnickv <nickvessa@gmail.com>
2022-12-24 20:22:04 -0500

    picklefinder

---
+++ picklefinder.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2 as cv 
+
+
+
+capture = cv.VideoCapture(0)
+
+if not capture.isOpened():
+    print("cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = capture.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+capture.release()
+cv.destroyAllWindows()
+
